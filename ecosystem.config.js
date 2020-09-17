4960e020b738a8c8c32f2f22f98105898343ae71
Michael Douglas Soares <michaeldouglas.10.94@gmail.com>
2020-09-16 23:42:50 -0300

    add config ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,13 @@ module.exports = {
     {
       name: 'api_keywords',
       script: 'dist/server.js',
-      instances: '2',
+      instances: '4',
       autorestart: true,
       watch: true,
       exec_mode: 'cluster',
+      log_date_format: 'yyyy-MM-dd HH:mm:ss',
+      error_file: '/var/logs/error_api_keywords_pm2.log',
+      out_file: '/var/logs/logs_api_keywords_pm2.log',
       env: {
         NODE_ENV: 'development',
         TZ: 'America/Sao_Paulo'
@@ -23,6 +26,9 @@ module.exports = {
       autorestart: true,
       watch: true,
       exec_mode: 'fork',
+      log_date_format: 'yyyy-MM-dd HH:mm:ss',
+      error_file: '/var/logs/error_job_api_keywords_pm2.log',
+      out_file: '/var/logs/logs_job_api_keywords_pm2.log',
       env: {
         NODE_ENV: 'development',
         TZ: 'America/Sao_Paulo'
